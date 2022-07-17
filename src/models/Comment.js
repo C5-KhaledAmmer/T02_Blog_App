6ff2bff552083b661f3b5af86a2a225e410fe6bb
KhaledAmmer <k.tayseer.amer@gmail.com>
2022-07-17 21:29:56 +0300

    Build Models

---
+++ src/models/Comment.js
@@ -0,0 +1,9 @@
+class Comments {
+    constructor(id,postId,name,email,body){
+        this.id = id;
+        this.name = name;
+        this.email =email;
+        this.body = body;
+        this.postId = postId;
+    }
+}
